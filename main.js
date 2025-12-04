23e5e75bfcc181e274f8a86fb1163db47a8e9e55
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-12-04 15:02:37 -0500

    Rename properties of gameBoard in main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // alert("main.js linked");
 
-gameBoard = (function createGameBoard (rows, columns) {
+gameBoard = (function createGameBoard (_rows, _columns) {
     /* 
         Abstraction: (rows) by (columns) game board. 
         Representation Invariant: 
@@ -8,33 +8,33 @@ gameBoard = (function createGameBoard (rows, columns) {
             each cell on the board must be a valid piece "", "X", or "O"
     */
 
-    let board = [];
-    const validPieces = ["", "X", "O"];
-    const debug = true;
+    let _board = [];
+    const _validPieces = ["", "X", "O"];
+    const _debug = true;
 
-    for (let i = 0; i < rows; i++){
-        board.push([]);
-        for (let j = 0; j < columns; j++){
-            board[i].push("");
+    for (let i = 0; i < _rows; i++){
+        _board.push([]);
+        for (let j = 0; j < _columns; j++){
+            _board[i].push("");
         }
         
     }
 
     const checkRepInv = () => {
         // Checks if the representation invariant is held, and prints findings to the console.
-        if (rows < 0 || columns < 0) {
+        if (_rows < 0 || _columns < 0) {
             console.warn("rows or columns negative");
         }
-        if (board.length != rows) {
+        if (_board.length != _rows) {
             console.warn("board's number of rows does not match rows variable");
         }
-        for(let i = 0; i < rows; i++){
-            if (board[i].length != columns) {
+        for(let i = 0; i < _rows; i++){
+            if (_board[i].length != _columns) {
                 console.warn(`the board's ${i+1}th row does not match with columns variable`);
             }
-            for(let j = 0; j < columns; j++){
-                if (!validPieces.includes(board[i][j])) {
-                    console.warn(`the cell at board[${i}][${j}] is not a valid piece: ${board[i][j]}`)
+            for(let j = 0; j < _columns; j++){
+                if (!_validPieces.includes(_board[i][j])) {
+                    console.warn(`the cell at board[${i}][${j}] is not a valid piece: ${_board[i][j]}`)
                 }
             }
         }
@@ -43,11 +43,11 @@ gameBoard = (function createGameBoard (rows, columns) {
 
     const printBoard = () => {
         // Prints the board to the console
-        for(let i = 0; i < rows; i++){
+        for(let i = 0; i < _rows; i++){
             let rowStr = ""
-            for(let j = 0; j < columns; j++){
-                rowStr += board[i][j];
-                if (debug) rowStr += `(${i},${j})`;
+            for(let j = 0; j < _columns; j++){
+                rowStr += _board[i][j];
+                if (debug) rowStr += ` (${i},${j})`;
                 rowStr += "\t";
             }
             console.log(rowStr + "\n");
