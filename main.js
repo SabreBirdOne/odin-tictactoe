47469bcec26202b046b2c5554527674ac65e5967
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-12-04 15:37:29 -0500

    Add playerFactory to main.js
    Fix the checkRepInv function in gameBoard
    Add test suite in main.js for gameBoard and
    playerFactory

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // alert("main.js linked");
 
-gameBoard = (function createGameBoard (_rows, _columns) {
+let gameBoard = (function createGameBoard (_rows, _columns) {
     /* 
         Abstraction: (rows) by (columns) game board. 
         Representation Invariant: 
@@ -20,18 +20,23 @@ gameBoard = (function createGameBoard (_rows, _columns) {
 
     const checkRepInv = () => {
         // Checks if the representation invariant is held, and prints findings to the console.
+        let repInvHeld = true;
         if (_rows < 0 || _columns < 0) {
             console.warn("rows or columns negative");
+            repInvHeld = false;
         }
         if (_board.length != _rows) {
             console.warn("board's number of rows does not match rows variable");
+            repInvHeld = false;
         }
         for(let i = 0; i < _rows; i++){
             if (_board[i].length != _columns) {
                 console.warn(`the board's ${i+1}th row does not match with columns variable`);
+                repInvHeld = false;
             }
         }
-        return console.log("Representation Invariant held");
+        
+        if (repInvHeld) console.log("Representation Invariant held");
     }
 
     const printBoard = () => {
@@ -40,7 +45,7 @@ gameBoard = (function createGameBoard (_rows, _columns) {
             let rowStr = ""
             for(let j = 0; j < _columns; j++){
                 rowStr += _board[i][j];
-                if (debug) rowStr += ` (${i},${j})`;
+                if (_debug) rowStr += ` (${i},${j})`;
                 rowStr += "\t";
             }
             console.log(rowStr + "\n");
@@ -49,3 +54,50 @@ gameBoard = (function createGameBoard (_rows, _columns) {
 
     return {printBoard, checkRepInv};
 })(3, 3);
+
+let playerFactory = function createPlayer (_piece, _name = "anonymous") {
+    /* 
+        Abstraction:
+            piece is the piece that the player will place on the board
+            name is the player name
+        Representation Invariant:
+            name and piece are strings
+    */
+
+    const checkRepInv = () => {
+        // Checks if the representation invariant is held, and prints findings to the console.
+        let repInvHeld = true;
+        if (!(typeof(_name) === "string" && typeof(_piece) === "string")){
+            console.warn("player name or piece are not instances of strings");
+            repInvHeld = false;
+        }
+        if (repInvHeld) console.log("Representation Invariant held");
+    }
+
+    const getName = () => _name.slice(0);
+    const setName = (newName) => _name = newName;
+    const getPiece = () => _piece.slice(0);
+
+    return {getName, setName, getPiece, checkRepInv};
+}
+
+let playerX = playerFactory("X");
+let playerO = playerFactory("O", "Omega");
+
+gameBoard.checkRepInv();
+gameBoard.printBoard();
+
+for (player of [playerX, playerO]){
+    player.checkRepInv();
+    console.log(player.getName());
+    console.log(player.getPiece());
+}
+
+playerX.setName("Xavier");
+playerO.setName("Olivia");
+
+for (player of [playerX, playerO]){
+    player.checkRepInv();
+    console.log(player.getName());
+    console.log(player.getPiece());
+}
