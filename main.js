9d36abb78d6825428899ee9dd4c564e3d4188ed1
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-12-04 16:54:35 -0500

    Add player and status functions to gameEngine.
    Update test suite for the added gameEngine
    functions.

--- main.js
+++ main.js
@@ -83,12 +83,75 @@ let playerFactory = function createPlayer (_piece, _name = "anonymous") {
     return {getName, setName, getPiece, checkRepInv};
 }
 
-let gameEngine = (function createGame(_gameBoard) {
+let gameEngine = (function createGame(_board, _piecesToWin) {
+    /*
+        Abstraction: 
+            board refers to the singleton gameBoard
+            players is a list of players in the game
+            playerTurn marks the player turn of the game
+            isRun indicates if the game is running or not.
+                if the game is running, new players cannot be added.
+            piecesToWin: number of pieces in a row or column to win.
+
+        Representation Invariant:
+            playerTurn must be in range [0, number of players - 1], like an array index.
+            isRun is a boolean
+            piecesToWin is a positive integer >= 2
+    */
     let _players = [];
-}) (gameBoard);
+    let _playerTurn = 0;
+    let _isRun = false;
+    // _board, _piecesToWin passed in by IIFE
+
+    const checkRepInv = () => {
+        // Checks if the representation invariant is held, and prints findings to the console.
+        let repInvHeld = true;
+        if (!(0 <= _playerTurn && _playerTurn <= Math.max(0, _players.length))){
+            console.log(`_playerTurn ${_playerTurn} not an index of players`);
+            repInvHeld = false;
+        }
+        if (typeof(_isRun) != 'boolean'){
+            console.log(`_isRun ${_isRun} is not a boolean`);
+            repInvHeld = false;
+        }
+        if (!(_piecesToWin >= 2 && Number.isInteger(_piecesToWin))){
+            console.log(`_piecesToWin ${_piecesToWin} is not positive integer >= 2`);
+            repInvHeld = false;
+        }
+
+        if (repInvHeld) console.log("Representation Invariant held");
+    }
+
+    // player related functions
+    const addPlayer = (player) => {
+        if (!_isRun){
+            _players.push(player);
+        }
+    }
+    const resetPlayerTurn = () => _playerTurn = 0;
+    
+    const printPlayerLog = () => {
+        console.log(`Current player turn: ${_playerTurn}`);
+        console.log("Players:");
+        for(let i = 0; i < _players.length; i++){
+            let logStr = "\t" + `${_players[i].getName()} (piece: ${_players[i].getPiece()})`;
+            if (i === _playerTurn) logStr += ' (current turn)';
+            console.log(logStr);        
+        }
+    }   
+
+    // game status related functions
+    const isGameRunning = () => _isRun;
+    const toggleRunGame = () => _isRun = !_isRun;
+
+    // board related functions
+    
+
+    return {addPlayer, resetPlayerTurn, printPlayerLog, isGameRunning, toggleRunGame, checkRepInv};
+}) (gameBoard, 3);
 
 // TEST SUITE
-
+console.log({isGameRunning: gameEngine.isGameRunning()});
 gameBoard.checkRepInv();
 gameBoard.printBoard();
 
@@ -98,8 +161,24 @@ let playerO = playerFactory("O", "Omega");
 playerX.setName("Xavier");
 playerO.setName("Olivia");
 
-for (player of [playerX, playerO]){
-    player.checkRepInv();
-    console.log(player.getName());
-    console.log(player.getPiece());
-}
+console.log("Testing isRun")
+console.log({isGameRunning: gameEngine.isGameRunning()});
+gameEngine.toggleRunGame();
+console.log({isGameRunning: gameEngine.isGameRunning()});
+gameEngine.checkRepInv();
+
+console.log("\t## Adding a player while game is running. Should not be allowed by game engine");
+gameEngine.addPlayer(playerX);
+gameEngine.printPlayerLog();
+
+console.log("\t## Stop the game from running and add 2 players.")
+gameEngine.toggleRunGame();
+gameEngine.addPlayer(playerX);
+gameEngine.addPlayer(playerO);
+gameEngine.printPlayerLog();
+console.log({isGameRunning: gameEngine.isGameRunning()});
+
+console.log("\t## Reset player turn test")
+gameEngine.resetPlayerTurn();
+gameEngine.printPlayerLog();
+gameEngine.checkRepInv();
